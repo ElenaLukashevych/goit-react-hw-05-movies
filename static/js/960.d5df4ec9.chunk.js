e765ca06623f884c79dc6e18dd458cad5a779baf
ElenaLukashevych <89803537+ElenaLukashevych@users.noreply.github.com>
2022-06-27 10:20:38 +0000

    Deploying to gh-pages from @ ElenaLukashevych/goit-react-hw-05-movies@d335093491daf4ee5ac6d9fc1985deef14096a05 🚀

---
+++ static/js/960.d5df4ec9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[960],{8960:function(e,t,r){r.r(t),r.d(t,{default:function(){return o}});var n=r(501),c=r(184);var o=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)("h2",{children:"not found"}),(0,c.jsx)(n.rU,{to:"/",children:"Back to home"})]})}}}]);
+//# sourceMappingURL=960.d5df4ec9.chunk.js.map
